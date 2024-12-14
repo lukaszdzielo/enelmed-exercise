dd8360807146b2e555a3b52c51e1844b25d01d23
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2024-12-14 01:33:19 +0100

    edit width of main container

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,17 @@ module.exports = {
 	content: ["./src/**/*.{html,js}", "./*.html"],
 	theme: {
 		extend: {},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+		},
 		container: {
 			center: true,
 			padding: {
 				DEFAULT: '16px',
+				md: '32px',
 			},
 		},
 	},
